28e2d89698e3e779cbadb0b9474b692b504f825f
Monica Howard <jay.m.howard@gmail.com>
2019-07-19 07:35:29 -0500

    creates Jumbotron.js and imports it into App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Nav from "./components/Nav.js";
+import Jumbotron from "./components/Jumbotron.js";
 
 function App() {
   return (
-    <div className="container">
-      <Nav />
+    <div className="container-fluid">
+      <div className="container">
+        <Nav />
+      </div>
+      <Jumbotron />
     </div>
   );
 }
---
+++ src/components/Jumbotron.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Jumbotron = () => {
+  return (
+    <div>
+      <div className="jumbotron jumbotron-fluid">
+        <div className="container">
+          <h1 className="display-4">
+            Hi, I'm Monica Howard, a full stack developer.
+          </h1>
+          <p className="lead">Keep scrolling to check out some of my work.</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Jumbotron;
