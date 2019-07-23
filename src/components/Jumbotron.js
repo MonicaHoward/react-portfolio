0176e3d7123f3122abfc1d930a98d9cec10acfbf
Monica Howard <jay.m.howard@gmail.com>
2019-07-22 22:12:47 -0500

    change font size on Jumbotron

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -6,7 +6,7 @@ const Jumbotron = () => {
     <div>
       <div className="jumbotron jumbotron-fluid dark-trans">
         <div className="container">
-          <h1 className="display-4">
+          <h1 className="display-5">
             Hi, I'm Monica Howard, a full stack developer.
           </h1>
           <p className="lead">Keep scrolling to check out some of my work.</p>
